44f9a738adff388bc57a99a0f7d08e52f8e8ff01
bintangraharja <bintang.raharja@student.umn.ac.id>
2020-12-03 21:50:35 +0700

    lanjut

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Header from './components/header.js';
 const name = "bintang";
 function App() {
   return (
     <div>
-      
+    <Header />
+    
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,2 +1,10 @@
 import React from 'react';
 
+const Header = () => {
+    return(
+    <section>
+        <h1>Weather</h1>
+    </section>
+    );
+}
+export default Header;
