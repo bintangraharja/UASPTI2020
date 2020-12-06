3227737137fc70d5d9bed70b82b112a4e4f29e6c
bintangraharja <bintang.raharja@student.umn.ac.id>
2020-12-06 20:10:08 +0700

    ganti class jadi fungsi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header.js';
 import Form from './components/form';
 import Weather from './components/weather';
 
 const api_key = "3088f17d218519ad800639fb54e469ed";
-class App extends React.Component {
-  state = {
+function App() {
+ const [state,setState] =useState({
     temperature: "",
     city:"",
     country:"",
@@ -15,8 +15,9 @@ class App extends React.Component {
     icon: "",
     description:"",
     error: ""
-  } 
-  getWeather = async(e) => {
+  });
+  const [ temp, setTemp] = useState("metric");
+  const getWeather = async(e) => {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     e.preventDefault();
@@ -27,12 +28,12 @@ class App extends React.Component {
     const lon = location.coord.lon;
     console.log(lat);
     console.log(lon);
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${temp}&appid=${api_key}`);
     const response = await api_call.json();
     console.log(response);
     console.log(api_call.ok);
     if(lat && lon){
-      this.setState({
+      setState({
         temperature: response.current.temp,
         city: city,
         country: country,
@@ -43,29 +44,36 @@ class App extends React.Component {
         error: " "
       })
     }else{
-      this.setState({
+      setState({
         error:"Please fill out input fields..."
       })
     }
   }
-  render(){return (
+  const getCel = () =>{
+    setTemp("metric")
+  }
+  const getFah =() =>{
+    setTemp("imperial")
+  }
+  return (
     <div className="container">
     <Header/>
     <h1>What's the waether like?</h1>
-    <Form loadWeather={this.getWeather}/>
+    <Form loadWeather={getWeather}/>
     <Weather
-     temperature= {this.state.temperature}
-     city= {this.state.city} 
-     country= {this.state.country}
-     humidity = {this.state.humidity}
-     pressure = {this.state.pressure}
-     icon= {this.state.icon}
-     description = {this.state.description}
-     error = {this.error}
+     temperature= {state.temperature}
+     city= {state.city} 
+     country= {state.country}
+     humidity = {state.humidity}
+     pressure = {state.pressure}
+     icon= {state.icon}
+     description = {state.description}
+     error = {state.error}
     />
     </div>
     
-  );}
+  );
 }
+
 
 export default App;
