bb9a9637b46102e128400237fe4a73d3851718ab
maureenzerl <maureen.zerlina@student.umn.ac.id>
2020-12-06 22:49:10 +0700

    fix image

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -9,40 +9,40 @@ function AboutUs(){
     <div className="ananda">
       <p>Ananda Setiyani</p>
       <p>00000037692</p>
-      <a href="https://github.com/AnandaSetiyani" target="_blank">
+      <a href="https://github.com/AnandaSetiyani" target="_blank" rel="noreferrer">
         <img className="logo" src={github} alt="github" ></img>
       </a><br/>
-      <a href="https://www.instagram.com/anandasf29/" target="_blank">
+      <a href="https://www.instagram.com/anandasf29/" target="_blank" rel="noreferrer">
         <img className="logo" src={ig} alt="github"></img>
       </a>
     </div>
     <div className="bintang">
       <p>Bintang Raharja</p>
       <p>00000037686</p>
-      <a href="https://github.com/bintangraharja" target="_blank">
+      <a href="https://github.com/bintangraharja" target="_blank" rel="noreferrer">
         <img className="logo" src={github} alt="github"></img>
       </a><br/>
-      <a href="https://www.instagram.com/bintangraharja_/" target="_blank">
+      <a href="https://www.instagram.com/bintangraharja_/" target="_blank" rel="noreferrer">
         <img className="logo" src={ig} alt="github"></img>
       </a>
     </div>
     <div className="maureen">
       <p>Maureen Zerlina O.</p>
       <p>00000036850</p>
-      <a href="https://github.com/maureenzerl" target="_blank">
+      <a href="https://github.com/maureenzerl" target="_blank" rel="noreferrer">
         <img className="logo" src={github} alt="github"></img>
       </a><br/>
-      <a href="https://www.instagram.com/maureenzerl/" target="_blank">
+      <a href="https://www.instagram.com/maureenzerl/" target="_blank" rel="noreferrer">
         <img className="logo" src={ig} alt="github"></img>
       </a>
     </div>
     <div className="rico">
       <p>Rico Ferdinand</p>
       <p>00000036905</p>
-      <a href="https://github.com/ricoferdinandd" target="_blank">
+      <a href="https://github.com/ricoferdinandd" target="_blank" rel="noreferrer">
         <img className="logo" src={github} alt="github"></img>
       </a><br/>
-      <a href="https://www.instagram.com/ricoferdinandd/" target="_blank">
+      <a href="https://www.instagram.com/ricoferdinandd/" target="_blank" rel="noreferrer">
         <img className="logo" src={ig} alt="github"></img>
       </a>
     </div>
