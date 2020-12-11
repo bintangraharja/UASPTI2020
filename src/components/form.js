e6d3de590a71f1a8fd6ce748998ce29ae43d01ea
bintangraharja <bintang.raharja@student.umn.ac.id>
2020-12-11 21:29:37 +0700

    first commit

--- src/components/form.js
+++ src/components/form.js
@@ -3,9 +3,8 @@ import React, { useState } from 'react';
 const Form = (props) =>{
 
     return(
-        <form onSubmit = {props.loadWeather} >
-            <input className="inputplace" type="text" name="city" placeholder="choose city" autoComplete="off"></input>
-             <button className="get">Get</button>
+        <form  >
+            <input className="inputplace" type="text" name="city" placeholder="choose city" autoComplete="off" onKeyPress={props.search}></input>
         </form>
     );
 }
