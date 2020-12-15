24e72488780f2353a4e501bebd4c83961b3065b4
bintangraharja <bintang.raharja@student.umn.ac.id>
2020-12-16 01:10:12 +0700

    tampilan default

--- src/pages/forecast.js
+++ src/pages/forecast.js
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchForecast } from '../api/forecast-api';
 import ForecastTiles from '../components/forecastTiles';
 import 'antd/dist/antd.css';
 import '../App.css';
 import { Row, Layout } from 'antd';
+import axios from 'axios';
+import {useCurrentLocation, geolocationOptions} from '../App'
 const { Content } = Layout;
 
 
 function Forecast(){
+  const { location , error} = useCurrentLocation(geolocationOptions);
   const [query,setQuery]= useState('');
   const [forecast,setFore]= useState({});
   
+  useEffect(()=>{
+    if(location){
+      const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=3088f17d218519ad800639fb54e469ed`;
+      const fetchData = async()=>{
+        await axios
+        .get(URL)
+        .then((response)=>{
+          setFore(response.data);
+        });
+      }
+      fetchData();
+    }
+    },[location, error]);
   
   const search = async(e) =>{
   if(e.key === 'Enter'){
