6e6510060355560fc8db0145248bff879a8960d2
maureenzerl <maureen.zerlina@student.umn.ac.id>
2020-12-16 00:58:47 +0700

    rapihin aboutus

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,54 +1,78 @@
 import React from 'react';
-import { Card, Col, Row } from 'antd';
+import { Col, Row } from 'antd';
 import github from '../components/aset/logo_github.png';
 import ig from '../components/aset/logo_ig.png';
+import nan from '../components/aset/nan.jpg';
+import bintang from '../components/aset/bintang.jpg';
+import maureen from '../components/aset/maureen.jpg';
+import rico from '../components/aset/rico.jpg';
 
 function AboutUs(){
     return(
       <div className="developer">
-        <h1>About Us</h1>
+        <div className="subtitle">About Us</div>
         <br></br>
+        <Row>
+        <div className="column">
         <div className="card">
-          <p>Ananda Setiyani</p>
-          <p>00000037692</p>
+          <img className="foto" src={nan} alt="nan"></img> <br></br>
+          <p>Ananda Setiyani<br></br>00000037692</p>
           <a href="https://github.com/AnandaSetiyani" target="_blank" rel="noreferrer">
             <img className="logo" src={github} alt="github" ></img>
-          </a><br/>
+          </a>
           <a href="https://www.instagram.com/anandasf29/" target="_blank" rel="noreferrer">
             <img className="logo" src={ig} alt="github"></img>
           </a>
         </div>
+        </div>
+        <div className="column">
         <div className="card">
-          <p>Bintang Raharja</p>
-          <p>00000037686</p>
+        <img className="foto" src={bintang} alt="bintang"></img> <br></br>
+          <p>Bintang Raharja <br></br> 00000037686</p>
           <a href="https://github.com/BintangRaharja" target="_blank" rel="noreferrer">
             <img className="logo" src={github} alt="github" ></img>
-          </a><br/>
+          </a>
           <a href="https://www.instagram.com/bintangraharja_/" target="_blank" rel="noreferrer">
             <img className="logo" src={ig} alt="github"></img>
           </a>
         </div>
+        </div>
+        <div className="column">
         <div className="card">
-          <p>Maureen Zerlina</p>
-          <p>00000036850</p>
+        <img className="foto" src={maureen} alt="maureen"></img> <br></br>
+          <p>Maureen Zerlina<br></br>00000036850</p>
           <a href="https://github.com/maureenzerl" target="_blank" rel="noreferrer">
             <img className="logo" src={github} alt="github" ></img>
-          </a><br/>
+          </a>
           <a href="https://www.instagram.com/maureenzerl/" target="_blank" rel="noreferrer">
             <img className="logo" src={ig} alt="github"></img>
           </a>
         </div>
+        </div>
+        <div className="column">
         <div className="card">
-          <p>Rico Ferdinand</p>
-          <p>00000036905</p>
-          <a href="https://github.com/ricoferdinand" target="_blank" rel="noreferrer">
-            <img className="logo" src={github} alt="github" ></img>
-          </a><br/>
+        <img className="foto" src={rico} alt="rico"></img><br></br>
+          <p>Rico Ferdinand<br></br>00000036905</p>
+          <a href="https://github.com/ricoferdinandd" target="_blank" rel="noreferrer">
+            <img className="logo" src={github} alt="github"></img>
+          </a>
           <a href="https://www.instagram.com/ricoferdinandd/" target="_blank" rel="noreferrer">
             <img className="logo" src={ig} alt="github"></img>
           </a>
         </div>
+        </div>
+        </Row><br/>
+        <div className="card1">
+        <h1><b>Special Thanks to:</b></h1>
+        <p>Andi Balo - 00000037809</p>
+        <p>htmlhints.com</p>
+        <p>norbertbartos.tech</p>
+        <p>openweathermap.org</p>
+        <p>unsplash.com</p>
+        <p>w3schools.com</p>
+        <p>youtube.com</p>
+        </div>
       </div>
-    );
+    )
 }
 export default AboutUs;
